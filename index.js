9b70646806dbf9f163f04a5495a68c8227ef958e
Greta Rakauskaite <95315989+GretaRa@users.noreply.github.com>
2025-10-03 15:59:44 +0200

    Update mobile/desktop view

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const userInputEl = document.getElementById("user-input")
+const convertBtn = document.getElementById("convert-btn")
+const lengthOutputEl = document.getElementById("length-output")
+const volumeOutputEl = document.getElementById("volume-output")
+const massOutputEl = document.getElementById("mass-output")
+
+lengthOutputEl.textContent = `20 meters = 65.616 feet | 20 feet = 6.096 meters`
+volumeOutputEl.textContent = `20 liters = 5.284 gallons | 20 gallons = 75.708 liters`
+massOutputEl.textContent = `20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos`
+
+function convert (input){
+    
+}
